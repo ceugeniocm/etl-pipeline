1b9fef41e82ba73325a95f3d05abb35a384ee929
Carlos Eugenio <ceugeniocm@hotmail.com>
2026-08-13 18:12:41 -0300

    Remove `checkpoint.json`, simplifica validações obrigatórias em `mapping.json` e corrige formatação de restrições em `create_tables.sql`.

--- create_tables.sql
+++ create_tables.sql
@@ -100,8 +100,8 @@ CREATE TABLE IF NOT EXISTS tb_agendamentos (
     FOREIGN KEY (tblproced_id) REFERENCES tb_procedimentos(tblproced_id),
     FOREIGN KEY (plano_id) REFERENCES tb_planos(plano_id),
     FOREIGN KEY (cli_id) REFERENCES tb_clinicas(cli_id),
---    FOREIGN KEY (sala_id) REFERENCES tb_salas(sala_id),
+    FOREIGN KEY (sala_id) REFERENCES tb_salas(sala_id),
     FOREIGN KEY (tpa_id) REFERENCES tb_tipos_agendamento(tpa_id),
---    FOREIGN KEY (tbl_idcid) REFERENCES tb_cids(tbl_idcid),
+    FOREIGN KEY (tbl_idcid) REFERENCES tb_cids(tbl_idcid),
     FOREIGN KEY (user_id) REFERENCES tb_usuarios(user_id)
 );
